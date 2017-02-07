6916c22bec15d092c5d87ce9485c6eba7f31916d
Quentin Gautreau <qgautreau@gmail.com>
2017-02-07 11:24:25 +0100

    fonction tourne cartes

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@ var boardElement = document.getElementById('board');
 var cardsNumber = 12;
 
 for (var i = 0; i < cardsNumber/2; i++) {
-    var cardElement = document.createElement("div");
-    cardElement.innerHTML = i;
-    cardElement.className = "card";
-    boardElement.appendChild(cardElement);
-    var cardElement = document.createElement("div");
-    cardElement.innerHTML = i;
-    cardElement.className = "card";
-    boardElement.appendChild(cardElement);
+    for (var j = 0; j < 2;j++) {
+        var cardElement = document.createElement("div");
+        cardElement.className = "card";
+
+        var valueElement = document.createElement("div");
+        valueElement.className = "value";
+        valueElement.innerHTML = i;
+
+        var hiddenElement = document.createElement("div");
+        hiddenElement.className = "hidden";
+
+        cardElement.appendChild(valueElement);
+        cardElement.appendChild(hiddenElement);
+        boardElement.appendChild(cardElement);
+
+        hiddenElement.addEventListener("click", function(){
+            this.style.display = "none";
+        });
+    }
 }
